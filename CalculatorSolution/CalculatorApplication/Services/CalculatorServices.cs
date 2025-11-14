63f8d82ace1fba614b39eacbc160751d8b3c9713
Harini Arumugam <Harini.Arumugam@LTTS.com>
2025-11-14 17:53:26 +0530

    Calculator Application commit with testcases

---
+++ CalculatorSolution/CalculatorApplication/Services/CalculatorServices.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculatorApplication.Services
+{
+    public class CalculatorServices
+    {
+            public int Add(int a, int b)
+            {
+                try
+                {
+                    return checked(a + b);
+            }
+                catch (Exception ex)
+                {
+                throw new InvalidOperationException("Overflow occurred during addition.");
+            }
+            }
+    }
+}
