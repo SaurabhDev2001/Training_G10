2a3cb28c05e57e895adcc9a272c37466ef395146
RathanMC <rathan.mc@ltts.com>
2025-11-27 15:26:58 +0530

    update

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 
 // Register your Calculator Service (Interface + Implementation)
 builder.Services.AddScoped<IAdditionService, AdditionService>();
-//builder.Services.AddScoped<ISubtractionService, SubtractionService>();
+builder.Services.AddScoped<ISubtractionService, SubtractionService>();
 
 // Swagger / OpenAPI settings
 builder.Services.AddEndpointsApiExplorer();
