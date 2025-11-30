522c6a485999c9668f7ee6fc4c7f61d87f534618
harini-ar <harini.arumugam@ltts.com>
2025-11-30 15:00:51 +0530

    Check

--- CalculatorApp/Models/CalculationRequest.cs
+++ CalculatorApp/Models/CalculationRequest.cs
@@ -2,6 +2,7 @@
 {
     public class CalculationRequest
     {
+        //Used to request a numbers in a structured format from the user
         public double Number1 { get; set; }
         public double Number2 { get; set; }
     }
